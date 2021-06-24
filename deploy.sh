d6bfd122689430f3178bdd2506b8c43f3a9c22a9
tetyana-zorya <tetyanazorya@gmail.com>
2021-06-24 09:53:36 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tetyana-zorya/pythos.git master:gh-pages
+git push -f git@github.com:tetyana-zorya/pythos.git master:main
 chmod +x deploy.sh
